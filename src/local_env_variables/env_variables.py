d343d8c20616c56a0b159fcd6d40b4e3bf16ad5b
Jackson Halpin <23747834+jacksonh1@users.noreply.github.com>
2024-01-25 13:33:01 -0500

    fixed env file stuff

--- src/local_env_variables/env_variables.py
+++ src/local_env_variables/env_variables.py
@@ -8,20 +8,24 @@ import pandas as pd
 from attrs import frozen
 from Bio import SeqIO
 
-# dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-src_dir = Path(os.path.dirname(__file__)).parent
-ROOT = src_dir.parent
-dotenv_path = ROOT / '.env'
+
+# src_dir = Path(os.path.dirname(__file__)).parent
+# ROOT = src_dir.parent
+# dotenv_path = ROOT / '.env'
+# dotenv.load_dotenv(dotenv_path)
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(dotenv_path)
+src_dir = Path(dotenv_path).parent.parent
 
 # this should be absolute path
 IUPRED_DIR = os.environ['IUPRED2A_LIB_DIR']
 
-phylogeny_lvl_ordering_file = ROOT / os.environ['PHYLOGENY_LVL_ORDERING']
+phylogeny_lvl_ordering_file = src_dir / os.environ['PHYLOGENY_LVL_ORDERING']
 with open(phylogeny_lvl_ordering_file, 'r') as f:
     PHYLOGENY_LVL_ORDERING = [l.strip() for l in f.readlines()]
 
-colormap_dir = ROOT / os.environ['COLORMAP_DIR']
+colormap_dir = src_dir / os.environ['COLORMAP_DIR']
 COLOR_MAP_FILES = {
     'clustal': colormap_dir / 'clustal_hex_map.json',
 }
--- src/local_env_variables/matrices.py
+++ src/local_env_variables/matrices.py
@@ -5,11 +5,18 @@ from pathlib import Path
 import dotenv
 import pandas as pd
 
-src_dir = Path(os.path.dirname(__file__)).parent
-ROOT = src_dir.parent
-dotenv_path = ROOT / '.env'
+# src_dir = Path(os.path.dirname(__file__)).parent
+# ROOT = src_dir.parent
+# dotenv_path = ROOT / '.env'
+# dotenv.load_dotenv(dotenv_path)
+# MATRIX_DIR = ROOT / os.environ['SUBSTITUTION_MATRIX_DIR']
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 dotenv.load_dotenv(dotenv_path)
-MATRIX_DIR = ROOT / os.environ['SUBSTITUTION_MATRIX_DIR']
+src_dir = Path(dotenv_path).parent.parent
+MATRIX_DIR = src_dir / os.environ['SUBSTITUTION_MATRIX_DIR']
+
+
 
 MATRIX_DF_DICT = {
     'BLOSUM62': MATRIX_DIR / "BLOSUM62.csv",
