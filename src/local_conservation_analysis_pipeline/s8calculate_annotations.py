a6a99d9dc1185a10c65ba3f05ba5cda5206dd944
Jackson Halpin <23747834+jacksonh1@users.noreply.github.com>
2024-01-30 17:52:05 -0500

    fixed issue with z score error

--- src/local_conservation_analysis_pipeline/s8calculate_annotations.py
+++ src/local_conservation_analysis_pipeline/s8calculate_annotations.py
@@ -42,22 +42,26 @@ def find_motif_regex(og: group_tools.ConserGene, regex):
     return matchseq, matchst, matchend
 
 
+def get_hit_aln_z_scores(lvlo: group_tools.LevelAlnScore):
+    hit_slice = slice(lvlo.hit_aln_start, lvlo.hit_aln_end + 1)
+    hit_z_scores = lvlo.z_scores[hit_slice]
+    hit_aln_seq = lvlo.query_aln_sequence[hit_slice]
+    nongap_inds = tools.get_non_gap_indexes(hit_aln_seq)
+    return list(np.array(hit_z_scores)[nongap_inds])
+
 def get_hit_aln_scores(lvlo: group_tools.LevelAlnScore):
     """
     returns a list of the scores and a list of the z scores for the hit (non-gap) positions in query sequence
     """
     hit_slice = slice(lvlo.hit_aln_start, lvlo.hit_aln_end + 1)
-    hit_z_scores = lvlo.z_scores[hit_slice]
     hit_scores = lvlo.scores[hit_slice]
     hit_aln_seq = lvlo.query_aln_sequence[hit_slice]
     nongap_inds = tools.get_non_gap_indexes(hit_aln_seq)
-    return list(np.array(hit_scores)[nongap_inds]), list(
-        np.array(hit_z_scores)[nongap_inds]
-    )
+    return list(np.array(hit_scores)[nongap_inds])
 
 
 def lvl_annotation_conservation_string(lvlo: group_tools.LevelAlnScore):
-    scores, z_scores = get_hit_aln_scores(lvlo)
+    z_scores = get_hit_aln_z_scores(lvlo)
     cons_str = cons_tools.conservation_string(
         z_scores, lvlo.hit_aln_sequence.replace('-', ''), z_score_cutoff=0.5
     )
@@ -110,12 +114,20 @@ def add_gene_annotations_2_dict(
             raise ValueError(f"level {level} already in d['level_annotations']")
         d['level_annotations'][level] = {}
         dlvl = d['level_annotations'][level]
-        hit_scores, hit_z_scores = get_hit_aln_scores(lvlo)
-        dlvl["hit_z_scores"] = hit_z_scores
-        dlvl["hit_mean_zscore"] = np.mean(hit_z_scores)
+        dlvl["aln_slice_file"] = lvl_annotation_aln_slice(lvlo)
+        hit_scores = get_hit_aln_scores(lvlo)
         dlvl["hit_scores"] = hit_scores
         dlvl["hit_mean_score"] = np.mean(hit_scores)
-        dlvl["aln_slice_file"] = lvl_annotation_aln_slice(lvlo)
+        if match[1] is not None:
+            regex_scores = hit_scores[match[1] : match[2] + 1]
+            dlvl["regex_match_scores"] = regex_scores
+            dlvl["regex_match_mean_score"] = np.mean(regex_scores)
+        if lvlo.z_score_failure is not None:
+            dlvl["z_score_failure"] = lvlo.z_score_failure
+            continue
+        hit_z_scores = get_hit_aln_z_scores(lvlo)
+        dlvl["hit_z_scores"] = hit_z_scores
+        dlvl["hit_mean_zscore"] = np.mean(hit_z_scores)
         dlvl["conservation_string"] = lvl_annotation_conservation_string(lvlo)
         if len(hit_z_scores) >= 5:
             dlvl["best mean z-score over 5 residue window"] = get_largest_avg_zscore_for_window(
@@ -126,9 +138,6 @@ def add_gene_annotations_2_dict(
                 hit_z_scores, window_size=10
             )
         if match[1] is not None:
-            regex_scores = hit_scores[match[1] : match[2] + 1]
-            dlvl["regex_match_scores"] = regex_scores
-            dlvl["regex_match_mean_score"] = np.mean(regex_scores)
             regex_z_scores = hit_z_scores[match[1] : match[2] + 1]
             dlvl["regex_match_zscores"] = regex_z_scores
             dlvl["regex_match_mean_zscore"] = np.mean(regex_z_scores)
