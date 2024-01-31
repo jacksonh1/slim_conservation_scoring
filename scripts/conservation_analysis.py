36d20c5fa4bccf9227642e82cf419e25df15b733
Jackson Halpin <23747834+jacksonh1@users.noreply.github.com>
2024-01-31 13:36:41 -0500

    made aln slice optional

--- scripts/conservation_analysis.py
+++ scripts/conservation_analysis.py
@@ -1,20 +1,35 @@
+import argparse
+import multiprocessing
+
 from local_conservation_analysis_pipeline import conservation_pipeline
 
-STEPS_TO_RUN = [
-    "s1setup_folder",
-    "multiprocess_steps",
-    "s8calculate_annotations",
-    "s9add_annotations2table",
-]
+N_CORES = round(multiprocessing.cpu_count()/2)
 
 
 if __name__ == '__main__':
     config_file = './params.yaml'
-    n_cores = 6
-    conservation_pipeline.main(config_file, n_cores, STEPS_TO_RUN)
-
-
-
+    parser = argparse.ArgumentParser(
+        description="Run conservation analysis pipeline",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        metavar="<file>",
+        required=True,
+        help="""path to config file in yaml format""",
+    )
+    parser.add_argument(
+        "-n",
+        "--n_cores",
+        type=int,
+        metavar="<int>",
+        default=N_CORES,
+        help=f"""number of cores to use""",
+    )
+    args = parser.parse_args()
+    conservation_pipeline.main(args.config, args.n_cores)
 
 
 
